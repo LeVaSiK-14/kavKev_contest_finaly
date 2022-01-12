66a4302d6cb8f3ced9637ccd032ca0db29640e73
LeVaSiK-14 <lev201611@gmail.com>
2022-01-12 07:17:29 +0000

    All finish without image in products

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from accounts.models import User, Contest
 
 
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('username', 'first_name', 'last_name')
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Contest)
 
